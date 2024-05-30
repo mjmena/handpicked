e2d6a593a70bb399f6a56c69fa1b88fd7b67403c
mjmena <martinjxm@gmail.com>
2024-05-30 00:48:48 -0400

    Simplify flake for dev shell only, add Tailwind

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! { <App/> }
+    })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <div class="h-screen flex items-center justify-center">
+            <div class="flex-1 bg-purple-200 rounded-fully">
+                <button
+                    on:click=move |_| { set_count.update(|n| *n+=1); }
+                    class:text-red-600=move || count() % 2 == 1
+                >
+                    "Click me: " {count}
+                </button>
+            </div>
+        </div>
+    }
 }
