147fbb47a80acd1cee6af9752f64ae614d36e974
mjmena <martinjxm@gmail.com>
2024-05-30 21:17:51 -0400

    Add a circle on click yay!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use leptos::*;
+use leptos_dom::logging::console_log;
 
 fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! { <App/> }
@@ -9,18 +11,36 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+    let (touches, set_touches) = create_signal(Vec::<TouchPoint>::new());
 
     view! {
-        <div class="h-screen flex items-center justify-center">
-            <div class="flex-1 bg-purple-200 rounded-fully">
-                <button
-                    on:click=move |_| { set_count.update(|n| *n+=1); }
-                    class:text-red-600=move || count() % 2 == 1
-                >
-                    "Click me: " {count}
-                </button>
-            </div>
+        <div class="h-screen flex items-center justify-center" on:click= move |event| {
+        console_log("help");
+        set_touches.update(|touches| {
+            touches.push(TouchPoint {
+                x: 0,
+                y: 0,
+                initial_time: chrono::Utc::now(),
+            });
+        });
+    }>
+            <For each=touches key=|touch| touch.initial_time children=move |touch|{
+                view!{<div class="flex-1"><Touch/></div>}
+            }/>
         </div>
     }
+}
+#[component]
+fn Touch() -> impl IntoView {
+    view! {
+         <svg height="100" width="100">
+          <circle r="45" cx="50" cy="50" fill="red" />
+        </svg>
+    }
+}
+#[derive(Clone)]
+struct TouchPoint {
+    x: isize,
+    y: isize,
+    initial_time: chrono::DateTime<chrono::Utc>,
 }
