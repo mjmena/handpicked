f0758523046a4018175b3a90fcbeb8cbc4ff4537
mjmena <martinjxm@gmail.com>
2024-10-12 03:22:36 -0400

    update rust and trunk version via flake.nix

--- flake.nix
+++ flake.nix
@@ -3,34 +3,35 @@
 
   inputs = {
     fenix = {
-      url = "github:nix-community/fenix";
+      url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, fenix, nixpkgs, flake-utils }:
+  outputs = {
+    fenix,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          toolchain = with fenix.packages.${system}; combine [
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = with fenix.packages.${system};
+          combine [
             default.toolchain
             targets.wasm32-unknown-unknown.latest.rust-std
-            rust-analyzer
           ];
-        in
-        {
-          devShells.default =
-            pkgs.mkShell {
-              packages = with pkgs; [
-                toolchain
-                trunk
-              ];
-
-            };
-        }
-      );
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            toolchain
+            trunk
+          ];
+        };
+      }
+    );
 }
-
