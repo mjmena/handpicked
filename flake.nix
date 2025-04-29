9cac03b24fdd0271a9bcd39fa97453fd788cc2bf
mjmena <martinjxm@gmail.com>
2025-04-29 02:10:08 -0400

    build: add build via flake and deploy to support using nix

--- flake.nix
+++ flake.nix
@@ -10,28 +10,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    fenix,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
+  outputs =
+    {
+      fenix,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-        toolchain = with fenix.packages.${system};
+        toolchain =
+          with fenix.packages.${system};
           combine [
             default.toolchain
             targets.wasm32-unknown-unknown.latest.rust-std
           ];
-      in {
+        nativeBuildInputs = [
+          toolchain
+          pkgs.trunk
+        ];
+      in
+      {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            toolchain
-            trunk
-          ];
+          inherit nativeBuildInputs;
         };
+        packages.githubPagesWasmBundle = pkgs.rustPlatform.buildRustPackage {
+          inherit nativeBuildInputs;
+          pname = "beerio";
+          version = "0.0.1";
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          buildPhase = "trunk build --release --public-url /handpicked";
+          installPhase = ''
+            cp dist/index.html dist/404.html
+            cp -r dist $out
+          '';
+        };
+
       }
     );
 }
